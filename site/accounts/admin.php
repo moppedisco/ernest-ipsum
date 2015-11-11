3abff919e33de00141ab9dad7ff8600bcc7a2622
Yacine Belarbi <yacbel@gmail.com>
2015-11-11 18:08:52 +0100

    first commit

---
+++ site/accounts/admin.php
@@ -0,0 +1,13 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: admin
+email: yacbel@gmail.com
+password: >
+  $2a$10$iQxdw5zVflMZIYziig0xzuUkjud0uajYIttDT0CndrvCLGZq0TzQi
+language: en
+role: admin
+history:
+  - about-ernest
+  - contact
+  - projects
+  - about
