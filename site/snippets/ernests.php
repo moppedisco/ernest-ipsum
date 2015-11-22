062647e84c6702f9c8b0067436e6165fbcf271fb
Yacine Belarbi <yacbel@gmail.com>
2015-11-22 20:20:23 +0100

    homepage colors

--- site/snippets/ernests.php
+++ site/snippets/ernests.php
@@ -1,11 +1,11 @@
 <ul class="teaser">
   <?php foreach(page('ernest')->children()->visible() as $project): ?>
-  <li class=" <?php echo 'teaser-item--'.$project->uid(); ?> ">
-    <div class="teaser__box">
+  <li class="teaser-item <?php echo 'teaser-item--'.$project->uid(); ?>">
+    <div class="teaser-item__box">
       <h3><a href="<?php echo $project->url() ?>"><?php echo $project->title()->html() ?></a></h3>
       <p><?php echo $project->intro() ?> </p>
     </div>
-    <a class="teaser__read-more" href="<?php echo $project->url() ?>">Get&nbsp;Ernest&nbsp;→</a>
+    <a class="teaser-item__read-more" href="<?php echo $project->url() ?>">Get&nbsp;Ernest&nbsp;→</a>
     <?php if($image = $project->images()->sortBy('sort', 'asc')->first()): ?>
       <img src="<?php echo $image->url() ?>" alt="<?php echo $project->title()->html() ?>" >
     <?php endif ?>
