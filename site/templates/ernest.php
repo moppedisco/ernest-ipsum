df96ea00c239d9c2564239e1564645b94256fe25
Yacine Belarbi <yacbel@gmail.com>
2015-11-22 22:56:18 +0100

    add header and text colors

--- site/templates/ernest.php
+++ site/templates/ernest.php
@@ -1,5 +1,9 @@
 <?php snippet('header') ?>
 
+  <?php if($prev = $page->prevVisible()): ?>
+  <a class="link--prev" href="<?php echo $prev->url() ?>">&larr;</a>
+  <?php endif ?>
+
   <main class="main" role="main">
 
     <div class="text">
@@ -9,5 +13,9 @@
     </div>
 
   </main>
+
+  <?php if($next = $page->nextVisible()): ?>
+  <a class="link--next" href="<?php echo $next->url() ?>">&rarr;</a>
+  <?php endif ?>
 
 <?php snippet('footer') ?>
