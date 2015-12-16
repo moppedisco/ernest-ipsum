010addfe89e369c52d21d9f925a79fa0774e01e8
Yacine Belarbi <yacbel@gmail.com>
2015-12-16 20:39:16 +0100

    shape animated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,67 @@
-$( document ).ready(function() {
+(function(window){
 
-  var randomSVG = Math.floor(Math.random() * 3);
+  var scrollPosition,
+      randomSVG = Math.floor(Math.random() * 3);
 
-  if($("body").hasClass("template--ernest")){
+  function init(){
+
+    if($("body").hasClass("template--ernest")){
+      randomShape();
+      paragraphSelector();
+    }
+
+    $(window).scroll(function () {
+      scrollPosition = $(window).scrollTop();
+      shapeParallax();
+    })
+  }
+
+  function randomShape(){
     $("[class^='image-shape']:eq( "+randomSVG+" )").show();
   }
 
-  // Paragraph buttons
-  $(".paragraph-selector li").on("click",function(){
-    $(".paragraph-selector li").removeClass("active");
-    $(this).toggleClass("active");
+  function paragraphSelector(){
+    var totalParagraphs = $(".paragraphs p").length;
 
-    var nrParagraphs = $(this).html();
+    // Check buttons
+    $(".paragraph-selector li").each(function(){
+      if($(this).index() < totalParagraphs){
 
-    $(".paragraphs p").each(function(){
-
-      if($(this).index() <= nrParagraphs){
-        $(this).fadeIn();
       } else {
-        $(this).fadeOut();
+        $(this).hide();
       }
     });
-  })
-});
+
+    // On click
+    $(".paragraph-selector li").on("click",function(){
+      $(".paragraph-selector li").removeClass("active");
+      $(this).toggleClass("active");
+
+      var nrParagraphs = $(this).html();
+
+      $(".paragraphs p").each(function(){
+
+        if($(this).index() <= nrParagraphs){
+          $(this).fadeIn();
+        } else {
+          $(this).fadeOut();
+        }
+      });
+    })
+  }
+
+  function shapeParallax(){
+    if(!$("body").hasClass("template--ernest")){
+      $(".image-shape--circle").css('transform', 'translateY(' + parseInt(scrollPosition / 10) + 'px)');
+      $(".image-shape--rectangle").css('transform', 'translateY(' + parseInt(-scrollPosition / 4) + 'px)');
+      $(".image-shape--triangle").css('transform', 'translateY(' + parseInt(-scrollPosition / 15) + 'px)');
+    }
+  }
+
+  window.Application = {
+    init: init
+  }
+
+}(window))  // Self execute
+
+Application.init();
