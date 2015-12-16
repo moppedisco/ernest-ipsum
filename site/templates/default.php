367d37ca400aa0b5c3951c5c5b9f2dad97934990
Yacine Belarbi <yacbel@gmail.com>
2015-12-16 23:05:26 +0100

    about page

--- site/templates/default.php
+++ site/templates/default.php
@@ -3,7 +3,7 @@
   <main class="main" role="main">
 
     <div class="text">
-      <h1><?php echo $page->title()->html() ?></h1>
+      <!-- <h1><?php echo $page->title()->html() ?></h1> -->
       <?php echo $page->text()->kirbytext() ?>
     </div>
 
